13dc74ebc7ecbd50294284ac2a9e722dae979225
Omar García López <omar.garcia@omargl.net>
2023-10-17 15:27:56 -0600

    Types of error

---
+++ cppFoundations/02FirstCppProgram/02FirstCppProgram.cpp
@@ -0,0 +1,17 @@
+//Import libraries
+#include <iostream>
+
+int main() {
+
+	//Compile time error
+	//std::cout << "¡Hola Evelyn!" << std::endl
+	std::cout << "¡Hola Evelyn desde c++ 20!" << std::endl;
+
+	/*
+	//Run time error
+	int valor = 7 / 0;
+	std::cout << "valor: " << valor << std::endl;
+	return 0;
+	*/
+
+}
